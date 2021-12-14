abe5384a20c1484452a71c5970f2d0320ba87988
Josephine Chong <josephine.chong@myob.com>
2021-12-14 13:16:27 +1100

    update tem teicked to tick  - present tese

--- main.js
+++ main.js
@@ -10,15 +10,20 @@ const setUp = () => {
   var breadButton = document.getElementById("bread-button");
   var eggs = document.getElementById("eggs");
   var eggsButton = document.getElementById("eggs-button");
-  
-  function ticked(btn, item){
+  var addMeButton = document.getElementById("add-me-button");
+  var newItem = document.getElementById("input");
+
+  function tick(btn, item){
       btn.disabled = true;
       item.className = "ticked";
     }
 
-  milkButton.onclick = function(){ticked(milkButton, milk)};
-  breadButton.onclick = function(){ticked(breadButton, bread)};
-  eggsButton.onclick = function(){ticked(eggsButton, eggs)};
+  milkButton.onclick = function(){tick(milkButton, milk)};
+  breadButton.onclick = function(){tick(breadButton, bread)};
+  eggsButton.onclick = function(){tick(eggsButton, eggs)};
+
+  addMeButton.onclick = function(){
+  }
 
   console.log("We're set up and ready to rumble!")
 };
